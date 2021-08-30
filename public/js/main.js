148447c0c00ef8a0b75e21735f5dad750adc88a2
cys0621 <cysyystiger@gmail.com>
2021-08-30 16:30:13 +0800

    myportfolio

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+	
+	// hide flash messages
+	setTimeout(function(){ $(".flashes").addClass("hidden")}, 2500);
+
+	// color the nav border
+	$(".nav").addClass(["one", "two", "three"][$(".selected").parent().index()]);
+
+	// filter projects
+	$(".category-filter .category").on("click", function(event) {
+
+		var selected = $(event.target).hasClass("active");
+
+		if (!selected) {
+			var category = $(event.target).attr("category");
+			$(".category-filter .category").removeClass("active");
+			$(event.target).addClass("active");
+			$(".project").fadeOut(300);
+			if (category == 'all') {
+				setTimeout(function(){
+					$(".project").fadeIn(300).css("display","inline-block");;
+				},300);
+			}
+			else {
+				setTimeout(function(){
+					$(".category-" + category).fadeIn(300).css("display","inline-block");;
+				},300);
+			}
+		}
+	});
+});
